fd054ec7856d89e77d6c0448e1a96483a2b9dc75
Yusuke KUOKA <ykuoka@gmail.com>
2019-06-25 10:11:28 +0900

    various fixes to make it work with private git repos as sources

--- pkg/helmfile-operator/cmd/cmd.go
+++ pkg/helmfile-operator/cmd/cmd.go
@@ -15,7 +15,7 @@ func Run() {
 
 	// The second argument to packr.New must be a local variable or a string literal
 	// in order for `packr2 build` to successfully determine the directory to be packed
-	box := packr.New("Bundled Appliance Assets", assetsDir)
+	box := packr.New("Bundled Helmfile Assets", assetsDir)
 
 	l := apputil.NewLogger(os.Stderr, "debug")
 	syncer, err := apputil.New(
@@ -30,6 +30,11 @@ func Run() {
 
 	var name = flag.String("name", "helmfile-operator", "Operator name included in log messages")
 	var configPath = flag.String("config", "", "Path to configuration file")
+	applierImage := flag.String("image", "", "container image for applier to be used in the IMAGE:TAG format")
+	applierImagePullPolicy := flag.String("image-pull-policy", "", "image pull policy for the applier image e.g. IfNotPresent, Always, etc.")
+	sshKeySecret := flag.String("ssh-key-secret", "", "Kubernetes secret containing SSH key for the `id_rsa`")
+	homeConfigMap := flag.String("home-configmap", "", "Kubernetes configmap containing applier's home-dir contents")
+	helmX := flag.Bool("helm-x", true, "Enable helm-x integration for kustomize, k8s manifests, patching support")
 
 	flag.Parse()
 
@@ -47,11 +52,20 @@ func Run() {
 
 	resource := schema.GroupVersionKind{
 		Group:   "apps.mumoshu.github.io",
-		Kind:    "Appliance",
+		Kind:    "Helmfile",
 		Version: "v1alpha1",
 	}
 
-	if err := controller_runtime.Run(*name, resource, controller_runtime.Conf(*configPath)); err != nil {
+	if err := controller_runtime.Run(
+		*name,
+		resource,
+		controller_runtime.Conf(*configPath),
+		controller_runtime.Image(*applierImage),
+		controller_runtime.ImagePullPolicy(*applierImagePullPolicy),
+		controller_runtime.SSHKeySecret(*sshKeySecret),
+		controller_runtime.HelmX(*helmX),
+		controller_runtime.HomeConfigMap(*homeConfigMap),
+	); err != nil {
 		fmt.Fprintf(os.Stderr, "Error: %v\n", err)
 		os.Exit(1)
 	}
